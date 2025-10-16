a14ba19fc7392e7662daad3d2a358011791b7dbf
hussain-sharif <99sharif00@gmail.com>
2025-10-16 15:17:59 +0530

    Shadcn Setup & Essential Comps are added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className='bg-accent'>
+      <h1 className="text-5xl text-amber-900">Hello AI World</h1>
+    </div>
   )
 }
 
